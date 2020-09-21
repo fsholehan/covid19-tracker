3cb1c2f95e839ee3ac60bb403ccee27b52b1c6b0
Your Name <you@example.com>
2020-09-21 11:03:28 +0700

    first commit

---
+++ src/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './Footer.css'
+function Footer() {
+  return (
+    <div className="footer">
+      <p><strong>2020</strong>. <a href="https://instagram.com/fsholehan" target="_blank">Fsholehan</a>.</p>
+    </div>
+  )
+}
+
+export default Footer
